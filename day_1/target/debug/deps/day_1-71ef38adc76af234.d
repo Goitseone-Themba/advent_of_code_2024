6ea755acf78ff81480e58019a59ed62706b5691b
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-02-05 12:25:17 +0200

    chore: clean up

--- day_1/target/debug/deps/day_1-71ef38adc76af234.d
+++
@@ -1,5 +0,0 @@
-/home/goitseone/Projects/Rust/advent_of_code_2024/day_1/target/debug/deps/libday_1-71ef38adc76af234.rmeta: src/main.rs
-
-/home/goitseone/Projects/Rust/advent_of_code_2024/day_1/target/debug/deps/day_1-71ef38adc76af234.d: src/main.rs
-
-src/main.rs:
